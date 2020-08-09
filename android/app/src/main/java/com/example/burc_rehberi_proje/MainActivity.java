b07c067597fa508c901485f1c5219e155a89a4a0
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-08-09 17:33:13 +0300

    initial_commit

---
+++ android/app/src/main/java/com/example/burc_rehberi_proje/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.burc_rehberi_proje;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Burç rehberi",
+      theme: ThemeData(
+        primarySwatch: Colors.pink
+      ),
+      home: ,
+    );
+  }
+
+}
